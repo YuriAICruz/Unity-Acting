ce86beddfe19c3170677f40da2aee083cbd3c487
Yuri Cruz <yuriaicruz@gmail.com>
2018-12-01 11:26:20 -0200

    add platform cat

---
+++ Graphene/Acting/Platformer/IActorController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Graphene.Acting.Platformer
+{
+    public interface IActorController
+    {
+        bool isServer { get; }
+        bool isClient { get; }
+        bool isLocalPlayer { get; }
+
+        event Action<Vector3> SetPosition;
+    }
+}
