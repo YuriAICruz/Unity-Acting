6fd94a6ccb923977e72fc9443f6aa10e26aad8d9
Yuri Cruz <yuriaicruz@gmail.com>
2019-06-21 01:03:29 -0300

    thitd person actor

---
+++ Graphene/Acting/ThirdPersonShooter/Player.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+using Graphene.InputManager;
+using UnityEngine;
+
+namespace Graphene.Acting.ThirdPersonShooter
+{
+    [RequireComponent(typeof(TpsInputManager))]
+    public class Player : ThirdPersonAhooterActor
+    {
+        private TpsInputManager _input;
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+
+            if (_actorController.isLocalPlayer)
+            {
+                if (_input == null)
+                    _input = GetComponent<TpsInputManager>();
+
+                _input.Init();
+
+                OnEnabled();
+            }
+
+            _input.Right_Axis += _camera.Aim;
+        }
+
+        protected override void OnEnabled()
+        {
+            base.OnEnabled();
+
+            if (_input == null)
+                return;
+
+            _input.Left_Axis += Move;
+            _input.Interact += Interact;
+            _input.Attack += Attack;
+            _input.AttackSeq += AttackSeq;
+            _input.Jump += Jump;
+            _input.Dodge += Dodge;
+            _input.LockOn += LockOn;
+            _input.LockOff += LockOff;
+        }
+
+        protected override void OnDisabled()
+        {
+            base.OnDisabled();
+
+            if (_input == null)
+                return;
+
+            _input.Left_Axis -= Move;
+            _input.Interact -= Interact;
+            _input.Attack -= Attack;
+            _input.AttackSeq -= AttackSeq;
+            _input.Jump -= Jump;
+            _input.Dodge -= Dodge;
+            _input.LockOn -= LockOn;
+            _input.LockOff -= LockOff;
+        }
+
+        private void Dodge()
+        {
+            _animation.Dodge();
+            _input.BlockInputs();
+            _physics.Dodge(_dodgeDuration, Speed * 2, () => { _input.UnblockInputs(); });
+        }
+
+        private void Attack()
+        {
+            if (_canInteract) return;
+            _animation.Attack();
+
+            if (_weapon)
+                _weapon.Use(0, 0.6f);
+        }
+
+        private void AttackSeq()
+        {
+            if (_canInteract) return;
+            _animation.AttackSeq();
+
+            if (_weapon)
+                _weapon.Use(0.2f, 0.3f);
+        }
+
+        private void Interact()
+        {
+            if (!_canInteract) return;
+            _animation.Interact();
+        }
+
+        private void Move(Vector2 dir)
+        {
+            var wdir = _camera.transform.TransformDirection(new Vector3(dir.x, 0, dir.y));
+            wdir.y = 0;
+            wdir.Normalize();
+            
+            _physics.Move(wdir, Speed);
+
+            var f = _camera.transform.forward;
+            f.y = 0;
+            Look(f);
+
+            _animation.SetSpeed(_physics.Speed());
+        }
+
+        protected override IEnumerator ReceiveDamage()
+        {
+            _input.BlockInputs();
+            Move(Vector3.zero);
+            yield return new WaitForSeconds(0.6f);
+            _input.UnblockInputs();
+        }
+        
+        protected virtual void LockOff()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected virtual void LockOn()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Graphene/Acting/ThirdPersonShooter/ThirdPersonAhooterActor.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections;
+using Graphene.Acting.ActorController;
+using Graphene.Acting.Collectables;
+using Graphene.Acting.Interfaces;
+using Graphene.Acting.Platformer;
+using Graphene.CameraManagement;
+using Graphene.Physics.Platformer;
+using UnityEngine;
+using BasicPhysics = Graphene.Physics.ThirdPersonShooter.BasicPhysics;
+
+namespace Graphene.Acting.ThirdPersonShooter
+{
+    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(CapsuleCollider))]
+    [RequireComponent(typeof(Animator))]
+    public class ThirdPersonAhooterActor : Actor
+    {
+        protected IActorController _actorController;
+
+        protected ShooterCameraBehavior _camera;
+
+        protected BasicPhysics _physics;
+
+        protected AnimationManager _animation;
+
+        public float Speed = 3;
+        public float JumpForce = 2;
+
+        [SerializeField] protected float _dodgeDuration;
+
+        protected bool _canInteract = false;
+        protected IInteractible _currentIntreactible;
+        protected bool _canClear;
+        
+        protected Weapon _weapon;
+
+        private void Awake()
+        {
+            _physics = GetComponent<BasicPhysics>();
+
+            _animation = new AnimationManager(GetComponent<Animator>());
+
+            _weapon = transform.GetComponentInChildren<Weapon>();
+            if (_weapon != null)
+            {
+                _weapon.SetOwner(this);
+            }
+
+            Life.Reset();
+            Life.OnDie += OnDie;
+
+            OnAwake();
+        }
+
+        protected virtual void OnAwake()
+        {
+        }
+
+        private void Start()
+        {
+            _actorController = Utils.InterfaceHelper.GetInterfaceComponent<IActorController>(this);
+
+            if (_actorController == null)
+            {
+                Debug.LogError("No IActorController found");
+                gameObject.SetActive(false);
+                return;
+            }
+
+            if (_actorController.isLocalPlayer)
+            {
+                OnEnable();
+            }
+
+            _camera = FindObjectOfType<ShooterCameraBehavior>();
+
+            if (_camera == null)
+            {
+                Debug.LogError("No CameraBehavior found");
+                return;
+            }
+
+            if (_actorController.isLocalPlayer)
+                _camera.SetTarget(this.transform);
+
+            OnStart();
+        }
+
+        protected virtual void OnStart()
+        {
+        }
+
+        protected virtual void OnEnabled()
+        {
+        }
+
+        protected virtual void OnDisabled()
+        {
+        }
+
+
+        private void OnEnable()
+        {
+            if (_actorController == null || !_actorController.isLocalPlayer) return;
+
+            _physics.JumpState += _animation.Jump;
+            _physics.GroundState += _animation.SetGroundState;
+
+            OnEnabled();
+        }
+
+        private void OnDisable()
+        {
+            if (_actorController == null || !_actorController.isLocalPlayer) return;
+
+            _physics.JumpState -= _animation.Jump;
+            _physics.GroundState -= _animation.SetGroundState;
+
+            OnDisabled();
+        }
+
+        protected void Look(Vector3 dir)
+        {
+            if (dir.magnitude <= 0) return;
+//
+//            var wdir = _camera.transform.TransformDirection(new Vector3(dir.x, 0, dir.y));
+//            wdir.y = 0;
+//            wdir.Normalize();
+
+            transform.rotation = Quaternion.LookRotation(dir);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            var col = other.transform.GetComponent<ICollectable>();
+            if (col != null)
+            {
+                col.Collect(this);
+            }
+
+            var intreactible = other.transform.GetComponent<IInteractible>();
+
+            if (intreactible != null)
+            {
+                _currentIntreactible = intreactible;
+                _canClear = false;
+            }
+        }
+
+        private void TouchWall(int side)
+        {
+            _animation.TouchWall(
+                new Vector2Int(
+                    side % 2 * ((side + 1) % 2 - side + 2),
+                    (side + 1) % 2 * Mathf.Min(1, side) * ((side + 1) % 2 - side + 2))
+            );
+        }
+
+        protected virtual void Jump()
+        {
+            _physics.Jump(JumpForce);
+        }
+
+        public override void DoDamage(int damage, Vector3 from)
+        {
+            StartCoroutine(ReceiveDamage());
+            Life.ReceiveDamage(damage);
+            _animation.ReceiveDamage();
+        }
+
+        protected virtual IEnumerator ReceiveDamage()
+        {
+            yield break;
+        }
+
+        protected virtual void OnDie()
+        {
+            Debug.Log(gameObject + "Died");
+            _animation.Die();
+        }
+
+        protected virtual void WallClimb(float height)
+        {
+            _physics.Climb(height, Speed);
+            _animation.Climb(height);
+        }
+
+
+        private void OnTriggerExit(Collider other)
+        {
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            var col = other.transform.GetComponent<ICollectable>();
+            if (col != null)
+            {
+                col.Collect(this);
+            }
+        }
+
+        private void OnCollisionExit(Collision other)
+        {
+        }
+
+        public void FootR()
+        {
+        }
+
+        public void FootL()
+        {
+        }
+
+        public void Hit()
+        {
+        }
+
+        public void Land()
+        {
+        }
+    }
+}
